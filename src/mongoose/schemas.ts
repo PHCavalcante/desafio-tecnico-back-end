c1076ccd53234e7dd2f4a9fa665177feb6a2ffaa
PHCavalcante <pedhcavdsan1603@gmail.com>
2026-01-03 22:21:51 -0300

    refactor: changes on schemas.ts

--- src/mongoose/schemas.ts
+++ src/mongoose/schemas.ts
@@ -18,12 +18,11 @@ const userMongooseModel = new mongoose.Schema({
   },
 });
 
-userMongooseModel.pre("save", async function (next) {
-  if (!this.isModified("password")) return next;
+userMongooseModel.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next;
 });
 
 userMongooseModel.methods.comparePassword = async function (
